3db40281a531c954e65db8eafddbd79507d5bbbf
Quang Lê <quanggr8@gmail.com>
2021-08-11 09:23:15 +0700

    rename Form1 to MainForm

--- Database Final Exam Rehearsal/MainForm.Designer.cs
+++ Database Final Exam Rehearsal/MainForm.Designer.cs
@@ -1,7 +1,7 @@
 ﻿
 namespace Database_Final_Exam_Rehearsal
 {
-    partial class Form1
+    partial class MainForm
     {
         /// <summary>
         /// Required designer variable.
--- Database Final Exam Rehearsal/Program.cs
+++ Database Final Exam Rehearsal/Program.cs
@@ -16,7 +16,7 @@ namespace Database_Final_Exam_Rehearsal
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new MainForm());
         }
     }
 }
